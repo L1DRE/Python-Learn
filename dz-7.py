465366796dd803ef826833586121838ee22d940b
L1DRE <namin9304@gmail.com>
2024-12-16 18:23:47 +0500

    dz-7.py

--- dz-7.py
+++ dz-7.py
@@ -1,1 +1,12 @@
+class IterableWithGenerator:
+    def __init__(self, data):
+        self.data = data
+    
+    def __iter__(self):
+        for item in self.data:
+            yield item
 
+iterable_object = IterableWithGenerator([1, 2, 3, 4, 5])
+
+for value in iterable_object:
+    print(value)
